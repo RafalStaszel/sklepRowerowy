3ec0cd5808a1cc844cb323c55f20de96c24942cd
Rafal <rafalpolo@gmail.com>
2017-03-18 19:17:41 +0100

    Zmiany

--- Gui.java
+++ Gui.java
@@ -11,76 +11,77 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 public class Gui extends JFrame implements ActionListener {
-	
-	
 
 	private JFrame frame;
 	private JMenuBar jmenu;
-	private JMenu jmPlik,jmOprogramie;
-	private JMenuItem mZapis,mWyjscie;
+	private JMenu jmPlik, jmOprogramie;
+	private JMenuItem mZapis, mWyjscie, mOdczyt, mOdczytZKlasy;
+	private JTextArea textArea;
+	JButton btnRowery, btnCzysc;
+	Magazyn magazyn = new Magazyn();
 	
-/*	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Gui window = new Gui();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
 
-	
-/*	public Gui() {
-		initialize();
-	}*/
-
-	
-	public Gui(){
+	public Gui() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 900, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setVisible(true);
-		
-		
+
 		jmenu = new JMenuBar();
 		frame.setJMenuBar(jmenu);
-		
+
 		jmPlik = new JMenu("Plik");
 		jmenu.add(jmPlik);
-		
+
 		mZapis = new JMenuItem("Zapis");
 		jmPlik.add(mZapis);
-		
+		mZapis.addActionListener(this);
+
+		mOdczyt = new JMenuItem("Odczyt");
+		jmPlik.add(mOdczyt);
+		mOdczyt.addActionListener(this);
+
+		mOdczytZKlasy = new JMenuItem("Odczyt z klasy");
+		jmPlik.add(mOdczytZKlasy);
+		mOdczytZKlasy.addActionListener(this);
+
 		mWyjscie = new JMenuItem("Wyjscie");
 		jmPlik.addSeparator();
 		jmPlik.add(mWyjscie);
-		
-		JButton btnRowery = new JButton("Rowery");
+
+		btnRowery = new JButton("Rowery");
 		btnRowery.setBounds(10, 60, 100, 20);
 		frame.getContentPane().add(btnRowery);
 		btnRowery.addActionListener(this);
 		
-		JTextArea textArea = new JTextArea();
+		btnCzysc = new JButton("Wyczyœæ");
+		btnCzysc.setBounds(200, 60, 100, 20);
+		frame.getContentPane().add(btnCzysc);
+		btnCzysc.addActionListener(this);
+
+		textArea = new JTextArea();
 		textArea.setBounds(50, 300, 800, 300);
 		frame.getContentPane().add(textArea);
-		
-		
-		
-		
-		Magazyn magazyn = new Magazyn();
-		magazyn.wstawRowery();
-		textArea.append(magazyn.stackPokazRowery());
-	
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object ae = e.getSource();
-		
-		
+		if (ae == btnRowery) {
+			textArea.setText("");
+			textArea.append(magazyn.stackPokazRowery());
+
+		} else if (ae == mZapis) {
+			magazyn.zapis();
+		} else if (ae == mOdczyt) {
+			magazyn.wstawRowery();
+		} else if (ae == mOdczytZKlasy){;
+		magazyn.odczytZKlasy();
 	}
+	else if(ae==btnCzysc){
+		textArea.setText(" ");
+	}
+}
 }
